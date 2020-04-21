21f186e008f988d61b1363054ad0800429e7dd98
Samuel Santana <samuel.ssa89@gmail.com>
2020-04-20 21:08:45 -0300

    :hammer: refactor scripts!

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response)=> {
-    return response.json({ message: 'Hello World'})
+    return response.json({ message: 'Hello GoStack'})
 });
 
 app.listen(3333, () => {
